d52fb7bae25bd66ee7779dcf6d97e05d2d49a33a
khanhtq2101 <khanh.tq190085@sis.hust.edu.vn>
2025-12-01 12:12:13 +0700

    pilot experiment kaggle

--- src/test_dataset.py
+++ src/test_dataset.py
@@ -33,7 +33,7 @@ if __name__ == '__main__':
     args.train_data_name = "iBims_Draft"
     args.test_data_name = "iBims_Draft"
     args.val_data_name = "iBims_Draft"
-    args.epochs = 1
+    args.epochs = 8 
     args.batch_size = 2
     
     data_name = args.train_data_name
