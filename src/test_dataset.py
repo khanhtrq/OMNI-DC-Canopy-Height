ced8a882977d85e2684f2ef883dd71b294487eb4
khanhtq2101 <khanh.tq190085@sis.hust.edu.vn>
2025-12-02 17:02:36 +0700

    preliminary experiment

--- src/test_dataset.py
+++ src/test_dataset.py
@@ -34,7 +34,7 @@ if __name__ == '__main__':
     args.test_data_name = "iBims_Draft"
     args.val_data_name = "iBims_Draft"
     args.epochs = 1
-    args.batch_size = 16
+    args.batch_size = 8
     args.val_depth_pattern = "3000"
     
     data_name = args.train_data_name
