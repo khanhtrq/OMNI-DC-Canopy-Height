2556b0af2c95f872f80480daa32d8e173454ebd7
khanhtq2101 <khanh.tq190085@sis.hust.edu.vn>
2026-01-01 12:08:30 +0700

    region

--- src/test_dataset.py
+++ src/test_dataset.py
@@ -40,7 +40,7 @@ if __name__ == '__main__':
     args.backbone_model = "rgbd"
     args.gpus = '0'
     # args.loss = '1.0*SeqL1+1.0*SeqL2+1.0*GradMatching+0.5*SeqLaplace'
-    args.loss = '1.0*SeqL1+1.0*SeqL2+0.5*SeqLaplace'
+    args.loss = '1.0*SeqL1+1.0*SeqL2+1.0*SeqLaplace'
     args.milestones = ''
 
     # args.pretrain = 'model_best_72epochs.pt'
