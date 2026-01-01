76f912142b4081ecc2aa4f0876a9f9f9c5060cf3
khanhtq2101 <khanh.tq190085@sis.hust.edu.vn>
2026-01-01 10:20:22 +0700

    loss

--- src/test_dataset.py
+++ src/test_dataset.py
@@ -35,12 +35,12 @@ if __name__ == '__main__':
     args.val_data_name = "iBims_Draft"
     args.epochs = 40
     args.batch_size = 8
-    args.val_depth_pattern = "30000"
+    args.val_depth_pattern = "60000"
     args.lr = 1e-5
     args.backbone_model = "rgbd"
     args.gpus = '0'
     # args.loss = '1.0*SeqL1+1.0*SeqL2+1.0*GradMatching+0.5*SeqLaplace'
-    args.loss = '1.0*SeqL1+1.0*SeqL2+1.0*GradMatching'
+    args.loss = '1.0*SeqL1+1.0*SeqL2+0.2*SeqLaplace'
     args.milestones = ''
 
     # args.pretrain = 'model_best_72epochs.pt'
