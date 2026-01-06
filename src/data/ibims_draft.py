3a4b2494afe1b98718b4d6915350904fb3ff28b1
khanhtq2101 <khanh.tq190085@sis.hust.edu.vn>
2026-01-06 18:25:07 +0700

    GEDI filter

--- src/data/ibims_draft.py
+++ src/data/ibims_draft.py
@@ -19,12 +19,13 @@ dataset_folder = "E:\CEI - Carbon Stock\experiments\data\IBims-1"
 split_txt = "E:\CEI - Carbon Stock\experiments\data\IBims-1\imagelist.txt"
 
 # gedi_folder = "/kaggle/input/gedi-canopy-height-hoanglien/canopy_data/GEDI"
-# sentinel_folder = "/kaggle/input/gedi-canopy-height-hoanglien/Sentinel-12band/Sentinel-12band"
+sentinel_folder = "/kaggle/input/gedi-canopy-height-hoanglien/Sentinel-12band/Sentinel-12band"
+
 gedi_folder = "/kaggle/input/gedi-canopy-height-hoanglien/GEDI_filtered/GEDI_filtered"
-sentinel_folder = "/kaggle/input/gedi-canopy-height-hoanglien/canopy_data/Sentinel"
+# sentinel_folder = "/kaggle/input/gedi-canopy-height-hoanglien/canopy_data/Sentinel"
 
 regions = ["HoangLien", "CucPhuong", "BaBe"]
-regions = ["BaBe"]
+regions = ["CucPhuong", "BaBe"]
 
 # gedi_folder = "E:\CEI - Carbon Stock\experiments\data\canopyheight_HoangLien\GEDI"
 # sentinel_folder = "E:\CEI - Carbon Stock\experiments\data\canopyheight_HoangLien\Sentinel"
