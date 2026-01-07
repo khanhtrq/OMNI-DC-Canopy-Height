fe285a69ada58136b4bcb049b829295c1ebcd59a
khanhtq2101 <khanh.tq190085@sis.hust.edu.vn>
2026-01-07 17:03:01 +0700

    RGB only

--- src/data/ibims_draft.py
+++ src/data/ibims_draft.py
@@ -19,10 +19,10 @@ dataset_folder = "E:\CEI - Carbon Stock\experiments\data\IBims-1"
 split_txt = "E:\CEI - Carbon Stock\experiments\data\IBims-1\imagelist.txt"
 
 # gedi_folder = "/kaggle/input/gedi-canopy-height-hoanglien/canopy_data/GEDI"
-sentinel_folder = "/kaggle/input/gedi-canopy-height-hoanglien/Sentinel-12band/Sentinel-12band"
-
 gedi_folder = "/kaggle/input/gedi-canopy-height-hoanglien/GEDI_filtered/GEDI_filtered"
-# sentinel_folder = "/kaggle/input/gedi-canopy-height-hoanglien/canopy_data/Sentinel"
+
+sentinel_folder = "/kaggle/input/gedi-canopy-height-hoanglien/canopy_data/Sentinel"
+# sentinel_folder = "/kaggle/input/gedi-canopy-height-hoanglien/Sentinel-12band/Sentinel-12band"
 
 regions = ["HoangLien", "CucPhuong", "BaBe"]
 regions = ["CucPhuong", "BaBe"]
