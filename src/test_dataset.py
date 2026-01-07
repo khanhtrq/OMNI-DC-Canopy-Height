524b3153bd5e31b56520bfa748bc81e27b41f3f2
khanhtq2101 <khanh.tq190085@sis.hust.edu.vn>
2026-01-07 14:59:01 +0700

    Sentinel only

--- src/test_dataset.py
+++ src/test_dataset.py
@@ -38,13 +38,13 @@ if __name__ == '__main__':
     args.val_depth_pattern = "60000"
     args.lr = 1e-5
     # args.backbone_mode = "rgbd"
-    args.backbone_mode = "sentinel"
+    args.backbone_mode = "sentineld"
     args.gpus = '0'
     # args.loss = '1.0*SeqL1+1.0*SeqL2+1.0*GradMatching+0.5*SeqLaplace'
     args.loss = '1.0*SeqL1+1.0*SeqL2'
     args.milestones = ''
 
-    # args.pretrain = 'model_best_72epochs.pt'
+    args.pretrain = 'model_best_72epochs.pt'
 
     #---------------
     #Draft Dec 24: Try to load trained model from Hugging Face
