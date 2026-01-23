0a7487f9bd604cf7c81b94654306950779b330cc
khanhtq2101 <khanh.tq190085@sis.hust.edu.vn>
2026-01-23 17:22:58 +0700

    30k points

--- src/main_canopy_height.py
+++ src/main_canopy_height.py
@@ -34,13 +34,13 @@ if __name__ == '__main__':
     args.val_data_name = "CanopyHeightDataset"
     args.epochs = 100
     args.batch_size = 8
-    args.val_depth_pattern = "60000"
+    args.val_depth_pattern = "30000"
     args.lr = 1e-4
     # args.backbone_mode = "rgbd"
     args.backbone_mode = "sentineld"
     args.gpus = '0'
     # args.loss = '1.0*SeqL1+1.0*SeqL2+1.0*GradMatching+0.5*SeqLaplace'
-    args.loss = '1.0*SeqL1+1.0*SeqL2+2.0*GradMatching'
+    args.loss = '1.0*SeqL1+1.0*SeqL2'
     args.milestones = [10, 20, 50]
     args.gamma = 0.2
 
