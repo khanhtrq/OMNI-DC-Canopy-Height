30a2be62b0056cef76ef1dbb6673a7c7ef086c82
khanhtq2101 <khanh.tq190085@sis.hust.edu.vn>
2026-01-28 09:48:28 +0700

    svd 5

--- src/main_canopy_height.py
+++ src/main_canopy_height.py
@@ -34,7 +34,7 @@ if __name__ == '__main__':
     args.val_data_name = "CanopyHeightDataset"
     args.epochs = 70
     args.batch_size = 8
-    args.val_depth_pattern = "45000"
+    args.val_depth_pattern = "60000"
     args.lr = 1e-4
     # args.backbone_mode = "rgbd"
     args.backbone_mode = "sentineld"
