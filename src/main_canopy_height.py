e1d8215159eb918040623371bee22429006e5e1f
khanhtq2101 <khanh.tq190085@sis.hust.edu.vn>
2026-02-03 13:08:05 +0700

    HL region

--- src/main_canopy_height.py
+++ src/main_canopy_height.py
@@ -41,7 +41,7 @@ if __name__ == '__main__':
     # args.loss = '1.0*SeqL1+1.0*SeqL2+1.0*GradMatching+0.5*SeqLaplace'
     args.loss = '1.0*SeqL1+1.0*SeqL2'
 
-    args.lr = 1e-4
+    args.lr = 1e-5
     # args.milestones = [10, 20, 50]
     args.milestones = []
     args.gamma = 0.2
