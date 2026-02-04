b891ad59d5866181c90a408ff09100aa8a027122
khanhtq2101 <khanh.tq190085@sis.hust.edu.vn>
2026-02-04 11:22:58 +0700

    hl 12 band

--- src/main_canopy_height.py
+++ src/main_canopy_height.py
@@ -32,7 +32,7 @@ if __name__ == '__main__':
     args.train_data_name = "CanopyHeightDataset"
     args.test_data_name = "CanopyHeightDataset"
     args.val_data_name = "CanopyHeightDataset"
-    args.epochs = 50
+    args.epochs = 100
     args.batch_size = 8
     args.val_depth_pattern = "60000"
     # args.backbone_mode = "rgbd"
