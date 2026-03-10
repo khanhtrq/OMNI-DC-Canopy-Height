75671c90468cda0e4a122a49bc9e9c793ed31146
khanhtq2101 <khanh.tq190085@sis.hust.edu.vn>
2026-03-10 10:52:41 +0700

    kochi

--- src/main_canopy_height.py
+++ src/main_canopy_height.py
@@ -33,9 +33,9 @@ if __name__ == '__main__':
     
     args.epochs = 200
     args.batch_size = 8
-    args.val_depth_pattern = "15000"
+    args.val_depth_pattern = "60000"
     # args.backbone_mode = "rgbd"
-    args.backbone_mode = "sentineld"
+    args.backbone_mode = "sentinel"
     args.gpus = '0'
     # args.loss = '1.0*SeqL1+1.0*SeqL2+1.0*GradMatching+0.5*SeqLaplace'
     args.loss = '1.0*SeqL1+1.0*SeqL2'
