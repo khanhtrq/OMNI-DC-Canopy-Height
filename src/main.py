87dae084eeb21de4f6c605f29ecf6be304616f2d
khanhtq2101 <khanh.tq190085@sis.hust.edu.vn>
2026-04-02 14:56:21 +0700

    correlation

--- src/main.py
+++ src/main.py
@@ -612,7 +612,7 @@ def test(args):
     print("Total valid points in prediction set:", len(y_pred))
 
     os.makedirs(f"{qualitative_path}/correlation", exist_ok=True)
-    correlation_heat_map(y_true, y_pred, f"{qualitative_path}/correlation/correlation_heatmap.png")
+    correlation_heat_map(np.array(y_true), np.array(y_pred), f"{qualitative_path}/correlation/correlation_heatmap.png")
 
     # writer_test.update(args.epochs, sample, output)
     writer_test.print_loss(args.epochs)
