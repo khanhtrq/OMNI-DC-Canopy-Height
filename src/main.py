31e7bc5febfe2b57656081542a7791c563bca09d
khanhtq2101 <khanh.tq190085@sis.hust.edu.vn>
2026-04-03 17:52:16 +0700

    correlation

--- src/main.py
+++ src/main.py
@@ -678,6 +678,8 @@ def correlation_heat_map(y_true, y_pred, name):
     print("Saving correlation heatmap with {} valid points.".format(len(y_true)))
     # Create 2D histogram
     bins = 10
+    bins = np.linspace(0, 50, 100)
+
     heatmap, xedges, yedges = np.histogram2d(y_true, y_pred, bins=bins)
     max_range = 45
     
