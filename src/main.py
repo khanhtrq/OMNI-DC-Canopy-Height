045054b08497c6f597278e7614d9380f04921936
khanhtq2101 <khanh.tq190085@sis.hust.edu.vn>
2026-04-03 14:32:36 +0700

    correlation

--- src/main.py
+++ src/main.py
@@ -712,7 +712,7 @@ def correlation_heat_map(y_true, y_pred, name):
 
     # plt.text(1, 25, f"MAE={mae:.1f}m\n$R^2$={r2:.2f}", fontsize=12)
 
-    plt.colorbar(label="Density")
+    # plt.colorbar(label="Density")
     plt.xlim(0, max_range)
     plt.ylim(0, max_range)
 
