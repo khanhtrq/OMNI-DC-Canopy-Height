0832b94522710b21085dbce66815b5da34aa7593
khanhtq2101 <khanh.tq190085@sis.hust.edu.vn>
2026-04-03 17:36:50 +0700

    correlation

--- src/main.py
+++ src/main.py
@@ -679,7 +679,7 @@ def correlation_heat_map(y_true, y_pred, name):
     # Create 2D histogram
     bins = 10
     heatmap, xedges, yedges = np.histogram2d(y_true, y_pred, bins=bins)
-    max_range = 50
+    max_range = 45
     
     cmap = plt.cm.YlGn  # or your cmap
 
